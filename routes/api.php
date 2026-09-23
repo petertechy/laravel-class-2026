3594e50f93b219e8a61df33480c1706c996062b1
Petertechy <petertechy01@gmail.com>
2026-09-23 12:07:06 +0100

    added api

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthenticateUser;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/', [UserController::class, 'index']);
+
+Route::get('/home', [UserController::class, 'home']);
+
+Route::get('/register-user', [UserController::class, 'registerPage']);
+
+Route::post('/register', [UserController::class, 'register']); 
+
+Route::get('/login', [UserController::class, 'loginPage'])->name('login');
+
+Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard')->middleware(AuthenticateUser::class);
+
+Route::post('logout', [UserController::class, 'logout']);
+
+Route::get('/product', [ProductController::class, 'index'])->name('product');
+
+Route::post('/create', [ProductController::class, 'create']);
+
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware(AuthenticateUser::class);
+
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->middleware(AuthenticateUser::class);
+
+Route::put('/users/{user}', [UserController::class, 'update'])->middleware(AuthenticateUser::class);
